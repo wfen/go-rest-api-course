e85712f6a9a006fcb2ac860eb30fd7a2bfe55f5c
Ward Fenton <ward.fenton@gmail.com>
2021-12-02 21:55:22 -0500

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/wfen/go-rest-api-course/internal/transport/http"
+)
 
 // App - the struct which contains things like pointers
 // to database connections
@@ -9,7 +14,18 @@ type App struct {}
 // Run - sets up our application
 func (app *App) Run() error {
 	fmt.Println("Setting Up Our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+	
+	return nil
 }
+
 func main() {
 	fmt.Println("Go REST API Course")
 	app := App{}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wfen/go-rest-api-course
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
